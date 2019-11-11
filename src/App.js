184902b8915ebb6f9f048dfbeea76b115841c6cf
Ebrahim Izadi <izadi.abraham@gmail.com>
2019-11-11 17:19:32 +0330

    the renderLayout function can render layout based on input. working on the number of rendering in the function

--- src/App.js
+++ src/App.js
@@ -3,39 +3,74 @@ import './App.scss';
 
 function App() {
 
+  function renderLayout() {
+
+    const text = "2sm/4l/xl";
+    let textArray = text.split("/");
+    let renderContent = "";
+    let numberOfRenderContent = text.match(/\d+/g).map(item => parseInt(item,10));
+
+    console.log(numberOfRenderContent);
+
+    for (let i = 0; i < textArray.length; i++) {
+      // console.log(textArray[i]);
+
+
+
+      switch (textArray[i]) {
+        case 'sm':
+          renderContent += "<div className=\"sm col-6\"></div>";
+          break;
+        case 'l':
+          renderContent += "<div className=\"l col-12\"></div>";
+          break;
+        case 'xl':
+          renderContent += "<div className=\"xl col-12\"></div>";
+          break;
+        default:
+          console.log('default case');
+
+      }
+    }
+    return <div className="container-fluid main" dangerouslySetInnerHTML={createMarkup(renderContent)}/>;
+
+
+
+  }
+
+  function createMarkup(stringInput) {
+    return {__html: stringInput};
+  }
+
   function sm() {
-    return(
+    return (
       <div className="sm col-6"></div>
     );
 
   }
 
   function l() {
-    return(
+    return (
       <div className="l col-12"></div>
     );
 
   }
 
   function xl() {
-    return(
+    return (
       <div className="xl col-12"></div>
     );
 
   }
 
 
-
   return (
-    <React.Fragment>
 
-      <div className="container-fluid row main">
-        {sm()}
-        {l()}
-        {xl()}
-      </div>
+    <div className="container-fluid row">
+      {renderLayout()}
+    </div>
 
-    </React.Fragment>
+
   );
 }
 
