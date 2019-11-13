b00335ed3b6b7d2a2e60c2e74a6e6eb609d70dbe
Ebrahim Izadi <izadi.abraham@gmail.com>
2019-11-13 12:43:54 +0330

    Updates

---
+++ precache-manifest.a35c0c931c8af64ac964f8271a4479b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b64e7087b58bd770ac8b2ceac9e306a",
+    "url": "/izadi-abraham/layout-generator.git/index.html"
+  },
+  {
+    "revision": "24baee48eaaa09f234d9",
+    "url": "/izadi-abraham/layout-generator.git/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "5425491b8bdc3a3ecea5",
+    "url": "/izadi-abraham/layout-generator.git/static/css/main.2a96c512.chunk.css"
+  },
+  {
+    "revision": "24baee48eaaa09f234d9",
+    "url": "/izadi-abraham/layout-generator.git/static/js/2.2c9e7973.chunk.js"
+  },
+  {
+    "revision": "5425491b8bdc3a3ecea5",
+    "url": "/izadi-abraham/layout-generator.git/static/js/main.93f48a9c.chunk.js"
+  },
+  {
+    "revision": "99728b25eb0e6dc13f04",
+    "url": "/izadi-abraham/layout-generator.git/static/js/runtime-main.76655102.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/izadi-abraham/layout-generator.git/precache-manifest.a35c0c931c8af64ac964f8271a4479b6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/izadi-abraham/layout-generator.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
